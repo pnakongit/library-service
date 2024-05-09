54c2c5388649234e7fb5cc70ae0a1aa6e4f7f5e5
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-05-09 15:35:32 +0300

    Set lifetime for access&refresh token and change auth header name

--- library_service/settings.py
+++ library_service/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +127,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZE",
+}
