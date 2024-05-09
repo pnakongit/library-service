8330d8f4232605921ee747215499adb5337d63f3
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-05-09 15:37:16 +0300

    Add endpoint for obtain token pair

--- user/urls.py
+++ user/urls.py
@@ -1,3 +1,8 @@
-urlpatterns = []
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+]
 
 app_name = "user"
