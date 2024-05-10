f68c7f235a69abe1f8e7f89d71cc152e8fc14bfe
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-05-10 17:01:13 +0300

    Register BookViewSet in urls.py in book app

--- book/urls.py
+++ book/urls.py
@@ -1,3 +1,10 @@
-urlpatterns = []
+from rest_framework import routers
+
+from book.views import BookViewSet
+
+router = routers.DefaultRouter()
+router.register("", BookViewSet, basename="book")
+
+urlpatterns = router.urls
 
 app_name = "book"
