2fbb5319421d4375992f6a6d655c8f05cc2e84a0
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-05-10 10:49:39 +0300

    Add endpoint for user registration

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from user.views import UserCreateAPIView
+
 urlpatterns = [
+    path("", UserCreateAPIView.as_view(), name="user_create"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
