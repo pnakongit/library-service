fb6680c5d54cb7934d7ddaf5067e4bca0ce04362
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-05-10 11:18:03 +0300

    Change path from api/v1/accounts/ to api/v1/users in library_service app

--- library_service/urls.py
+++ library_service/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/accounts/", include("user.urls", namespace="user")),
+    path("api/v1/users/", include("user.urls", namespace="user")),
 ]
