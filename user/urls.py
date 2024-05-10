3c2bae1b6d4969586a132b85140e8027a2e6dc6c
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-05-10 11:01:18 +0300

    Add endpoint for manage user

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from user.views import UserCreateAPIView
+from user.views import UserCreateAPIView, UserManageAPIView
 
 urlpatterns = [
     path("", UserCreateAPIView.as_view(), name="user_create"),
+    path("me/", UserManageAPIView.as_view(), name="me"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
+from user.models import User
 from user.serializers import UserSerializer
 
 
@@ -9,3 +10,11 @@ class UserCreateAPIView(generics.CreateAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+class UserManageAPIView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self) -> User:
+        return self.request.user
