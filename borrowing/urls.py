437558b836ef5261941a72a087ae0afccc565512
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-05-11 20:55:12 +0300

    Add borrowing list&retrieve endpoints, using BorrowingViewSet

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,3 +1,10 @@
-urlpatterns = []
+from rest_framework import routers
+
+from borrowing.views import BorrowingViewSet
+
+router = routers.DefaultRouter()
+router.register("", BorrowingViewSet, basename="borrowing")
+
+urlpatterns = router.urls
 
 app_name = "borrowing"
