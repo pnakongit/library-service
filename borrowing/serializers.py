e0b5c17fcf1aeba949dc941dcc3ab750608f43c8
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-05-12 16:21:51 +0300

    Add BorrowingCreateSerializer to serializers.py in borrowing app

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+
+from django.db import transaction
 from rest_framework import serializers
 
+from book.models import Book
 from book.serializers import BookSerializer
 from borrowing.models import Borrowing
 
@@ -19,3 +23,38 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "user",
             "book",
         ]
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Borrowing
+        fields = [
+            "id",
+            "book",
+            "user",
+            "expected_return_date",
+        ]
+
+    def validate_expected_return_date(self, value: datetime.date) -> datetime.date:
+        if value <= datetime.today().date():
+            raise serializers.ValidationError(
+                "Expected return date must be greater than current date"
+            )
+        return value
+
+    def validate_book(self, value: Book) -> Book:
+        if value.inventory < 1:
+            raise serializers.ValidationError(
+                f"{value.title.lower()} is not available for borrowing."
+            )
+        return value
+
+    def create(self, validated_data: dict) -> Borrowing:
+        with transaction.atomic():
+            borrowing = super().create(validated_data)
+            book = borrowing.book
+            book.inventory -= 1
+            book.save()
+        return borrowing
