0656270e69fdddcabd039dda7fc073df42395d64
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-05-12 20:00:48 +0300

    Add BorrowingFilter to filters.py in borrowing app

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,10 +1,11 @@
 from typing import Type
 
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.serializers import Serializer
 
-from borrowing.filters import IsBorrowingOwnerOrIsAdminFilterBackend
+from borrowing.filters import IsBorrowingOwnerOrIsAdminFilterBackend, BorrowingFilter
 from borrowing.models import Borrowing
 from borrowing.serializers import BorrowingSerializer, BorrowingCreateSerializer
 
@@ -19,7 +20,9 @@ class BorrowingViewSet(
     permission_classes = [IsAuthenticated]
     filter_backends = [
         IsBorrowingOwnerOrIsAdminFilterBackend,
+        DjangoFilterBackend,
     ]
+    filterset_class = BorrowingFilter
 
     def get_serializer_class(self) -> Type[Serializer]:
         if self.action == "create":
