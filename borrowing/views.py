3c667a8abb2ac83d16f90638a1deff71e67f5cda
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-05-12 18:53:36 +0300

    Add IsBorrowingOwnerOrIsAdminFilterBackend to filter_backends in BorrowingViewSet

--- borrowing/views.py
+++ borrowing/views.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.serializers import Serializer
 
+from borrowing.filters import IsBorrowingOwnerOrIsAdminFilterBackend
 from borrowing.models import Borrowing
 from borrowing.serializers import BorrowingSerializer, BorrowingCreateSerializer
 
@@ -16,6 +17,9 @@ class BorrowingViewSet(
 ):
     queryset = Borrowing.objects.all()
     permission_classes = [IsAuthenticated]
+    filter_backends = [
+        IsBorrowingOwnerOrIsAdminFilterBackend,
+    ]
 
     def get_serializer_class(self) -> Type[Serializer]:
         if self.action == "create":
