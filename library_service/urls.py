f47c1b7fdd35c7a6c083b12446a2ce89fd06c601
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-05-16 18:37:36 +0300

    Add url paths for shhema & swagger in main URL conf

--- library_service/urls.py
+++ library_service/urls.py
@@ -17,11 +17,18 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/accounts/", include("user.urls", namespace="user")),
     path("api/v1/books/", include("book.urls", namespace="book")),
     path("api/v1/borrowings/", include("borrowing.urls", namespace="borrowing")),
+    path("api/v1/docs/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/v1/docs/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger",
+    ),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
